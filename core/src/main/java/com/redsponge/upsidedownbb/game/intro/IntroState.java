6ab7cb8d4510bc18dd4359ff919cc53adb64847e
RedSponge <eran.godsi2003@gmail.com>
2019-04-28 17:04:01 +0300

    fix typo again

--- core/src/main/java/com/redsponge/upsidedownbb/game/intro/IntroState.java
+++ core/src/main/java/com/redsponge/upsidedownbb/game/intro/IntroState.java
@@ -2,7 +2,7 @@ package com.redsponge.upsidedownbb.game.intro;
 
 public enum IntroState {
 
-    PART_1("{SLOW}This is {VAR=playerName},{WAIT} They are a warrior who possesses a very{COLOR=CYAN}{WAVE} powerful item"),
+    PART_1("{SLOW}This is {VAR=playerName},{WAIT} They were a warrior who possessed a very{COLOR=CYAN}{WAVE} powerful item"),
     PART_2("{SLOW}A {WAVE}{COLOR=CYAN}gravity shirt{ENDWAVE}{CLEARCOLOR}, which gives its owner the ability to switch gravity!{FAST}\n (kinda self explanatory :P)"),
     PART_3("{SLOW}They searched the land looking for worthy foes{WAIT}.{WAIT}.{WAIT}."),
     PART_4("{SLOW}And finally{WAIT}.{WAIT}.{WAIT}. they found one.")
